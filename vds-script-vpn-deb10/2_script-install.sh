d7187c74a38c84847d068dddbdf552b62064f7d2
Krushon <email5000@yandex.ru>
2019-11-21 09:11:25 +0300

    Оптимизация под deb10

--- vds-script-vpn-deb10/2_script-install.sh
+++ vds-script-vpn-deb10/2_script-install.sh
@@ -10,10 +10,8 @@ systemctl enable chrony
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 # Готовимся к генерации сертификатов
 mkdir /etc/openvpn/easy-rsa
-cp -r /usr/share/easy-rsa/* /etc/openvpn/easy-rsa
-ln -s /etc/openvpn/easy-rsa/openssl-1.0.0.cnf /etc/openvpn/easy-rsa/openssl.cnf
-# Разрешаем форвардить пакеты из одной сети в другую.
-sed -i '28s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+# Разрешаем переcылать пакеты из одной сети в другую.
+sed -i '28s/.*net.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 echo
 echo -e "***** Script \033[33;1m2\033[0m of \033[33;1m3\033[0m COMPLETED in $SECONDS seconds *****"
 echo
