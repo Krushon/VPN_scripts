7e62b20f52f0479673d9b92230ccc3d6def2f655
Krushon <email5000@yandex.ru>
2019-11-29 14:40:05 +0300

    Добавлен вывод о настройке почты

--- openvpn-server-script/2_script-install.sh
+++ openvpn-server-script/2_script-install.sh
@@ -3,8 +3,9 @@ SECONDS=0
 printf "\033c"
 echo "Install requirements packets..."
 apt-get update
-apt-get install openvpn mc chrony nftables mutt mailutils -y
+apt-get install openvpn mc chrony nftables mutt mailutils gpgsm -y
 apt-get autoclean && apt-get clean
+apt-mark auto gpgsm
 systemctl enable chrony
 # Изменяем временную зону
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
@@ -12,6 +13,8 @@ ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 mkdir /etc/openvpn/easy-rsa
 # Разрешаем переcылать пакеты из одной сети в другую.
 sed -i '28s/.*net.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+echo
+echo -en "\e[1;31mДля отправки почты необходимо настроить /etc/exim4/update-exim4.conf.conf\e[0m"
 echo
 echo -e "***** Script \033[33;1m2\033[0m of \033[33;1m3\033[0m COMPLETED in $SECONDS seconds *****"
 echo
