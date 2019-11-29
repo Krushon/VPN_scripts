0425ec58f09965b8c399951e21cc66529ff3163a
Krushon <email5000@yandex.ru>
2019-11-29 14:38:49 +0300

    Обновление настроек ssh

--- openvpn-server-script/0_script-prepare.sh
+++ openvpn-server-script/0_script-prepare.sh
@@ -19,24 +19,57 @@ fi
 mv /etc/motd /etc/motd.bak
 touch /etc/motd && chmod 664 /etc/motd
 
-#### Раскомментируйте этот блок, если хотите подключаться к серверу по ключу
+# Настройка ssh
+## Можно выбрать один из предложенных блоков с настройками ssh.
+
+#### 1. Это блок по умолчанию для локальной машины. Закомментируйте этот блок 1, и расскомментируйте блок 2,если хотите подключаться к серверу по ключу.
+# Генерация файла sshd_config для доступа по ssh-ключу
+mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+touch /etc/ssh/sshd_config
+echo -en "Port 22\nAddressFamily inet\nProtocol 2\nDebianBanner no\nPrintMotd no\n" >> /etc/ssh/sshd_config
+echo -en "SyslogFacility AUTH\nLogLevel VERBOSE\nLoginGraceTime 15\nStrictModes yes\n" >> /etc/ssh/sshd_config
+echo -en "PrintLastLog yes\nKexAlgorithms diffie-hellman-group14-sha1\nHostKey /etc/ssh/ssh_host_rsa_key\n" >> /etc/ssh/sshd_config
+echo -en "HostKey /etc/ssh/ssh_host_dsa_key\nHostKey /etc/ssh/ssh_host_ecdsa_key\n" >> /etc/ssh/sshd_config
+echo -en "HostKey /etc/ssh/ssh_host_ed25519_key\nIgnoreRhosts yes\nPermitEmptyPasswords no\n" >> /etc/ssh/sshd_config
+echo -en "PermitRootLogin yes\nHostbasedAuthentication no\nChallengeResponseAuthentication no\n" >> /etc/ssh/sshd_config
+echo -en "KerberosAuthentication no\nGSSAPIAuthentication no\nGSSAPICleanupCredentials yes\n" >> /etc/ssh/sshd_config
+echo -en "UsePAM yes\nX11DisplayOffset 10\nX11Forwarding yes\nX11UseLocalhost no\n" >> /etc/ssh/sshd_config
+echo -en "TCPKeepAlive yes\nUsePrivilegeSeparation yes\nPermitUserEnvironment no\n" >> /etc/ssh/sshd_config
+echo -en "ClientAliveCountMax 0\nUseDNS no\nMaxStartups 10:50:30\n" >> /etc/ssh/sshd_config
+echo -en "AcceptEnv LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES\n" >> /etc/ssh/sshd_config
+echo -en "AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT\n" >> /etc/ssh/sshd_config
+echo -en "AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE\nAcceptEnv XMODIFIERS\n" >> /etc/ssh/sshd_config
+echo -en "Subsystem sftp /usr/lib/openssh/sftp-server\nCompression no\nMACs hmac-sha2-256\n" >> /etc/ssh/sshd_config
+chmod 644 /etc/ssh/sshd_config
+####
+
+#### 2. Раскомментируйте этот блок, если хотите подключаться к серверу по ключу
+# Генерация файла sshd_config для доступа по ssh-ключу
 #mkdir /root/.ssh
 #mv /root/authorized_keys /root/.ssh
 #chmod 700 /root/.ssh
 #chmod 600 /root/.ssh/authorized_keys
-# Генерация файла sshd_config для доступа по ssh-ключу
 #mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 #touch /etc/ssh/sshd_config
-#echo -en "Port 22\nProtocol 2\nHostKey /etc/ssh/ssh_host_rsa_key\nHostKey /etc/ssh/ssh_host_dsa_key\n" >> /etc/ssh/sshd_config
-#echo -en "HostKey /etc/ssh/ssh_host_ecdsa_key\nHostKey /etc/ssh/ssh_host_ed25519_key\nUsePrivilegeSeparation yes\n" >> /etc/ssh/sshd_config
-#echo -en "KeyRegenerationInterval 3600\nServerKeyBits 1024\nSyslogFacility AUTH\nLogLevel INFO\nLoginGraceTime 120\n" >> /etc/ssh/sshd_config
-#echo -en "StrictModes yes\nRSAAuthentication yes\nPubkeyAuthentication yes\nAuthorizedKeysFile      %h/.ssh/authorized_keys\n" >> /etc/ssh/sshd_config
-#echo -en "RhostsRSAAuthentication no\nIgnoreRhosts yes\nHostbasedAuthentication no\nPermitEmptyPasswords no\nChallengeResponseAuthentication no\n" >> /etc/ssh/sshd_config
-#echo -en "X11Forwarding yes\nX11DisplayOffset 10\nPrintMotd no\nPrintLastLog yes\nTCPKeepAlive yes\nAcceptEnv LANG LC_*\n" >> /etc/ssh/sshd_config
-#echo -en "Subsystem sftp /usr/lib/openssh/sftp-server\nUsePAM yes\n" >> /etc/ssh/sshd_config
+#echo -en "Port 22\nAddressFamily inet\nProtocol 2\nDebianBanner no\nPrintMotd no\n" >> /etc/ssh/sshd_config
+#echo -en "SyslogFacility AUTH\nLogLevel VERBOSE\nLoginGraceTime 15\nStrictModes yes\n" >> /etc/ssh/sshd_config
+#echo -en "PrintLastLog yes\nKexAlgorithms diffie-hellman-group14-sha1\nHostKey /etc/ssh/ssh_host_rsa_key\n" >> /etc/ssh/sshd_config
+#echo -en "HostKey /etc/ssh/ssh_host_dsa_key\nHostKey /etc/ssh/ssh_host_ecdsa_key\n" >> /etc/ssh/sshd_config
+#echo -en "HostKey /etc/ssh/ssh_host_ed25519_key\nPubkeyAuthentication yes\n" >> /etc/ssh/sshd_config
+#echo -en "AuthorizedKeysFile %h/.ssh/authorized_keys\nIgnoreRhosts yes\n" >> /etc/ssh/sshd_config
+#echo -en "PermitEmptyPasswords no\nHostbasedAuthentication no\nChallengeResponseAuthentication no\n" >> /etc/ssh/sshd_config
+#echo -en "KerberosAuthentication no\nGSSAPIAuthentication no\nGSSAPICleanupCredentials yes\n" >> /etc/ssh/sshd_config
+#echo -en "UsePAM yes\nX11DisplayOffset 10\nX11Forwarding yes\nX11UseLocalhost no\nTCPKeepAlive yes\n" >> /etc/ssh/sshd_config
+#echo -en "UsePrivilegeSeparation yes\nPermitUserEnvironment no\nClientAliveCountMax 0\n" >> /etc/ssh/sshd_config
+#echo -en "UseDNS no\nMaxStartups 10:50:30\n" >> /etc/ssh/sshd_config
+#echo -en "AcceptEnv LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES\n" >> /etc/ssh/sshd_config
+#echo -en "AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT\n" >> /etc/ssh/sshd_config
+#echo -en "AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE\nAcceptEnv XMODIFIERS\n" >> /etc/ssh/sshd_config
+#echo -en "Subsystem sftp /usr/lib/openssh/sftp-server\nCompression no\nMACs hmac-sha2-256\n" >> /etc/ssh/sshd_config
 #chmod 644 /etc/ssh/sshd_config
 ####
 
+# Скачиваем скрипты и даём права на запуск
 #wget https://raw.githubusercontent.com/Krushon/VPN_scripts/master/openvpn-server-script/0_script-prepare.sh
 wget https://raw.githubusercontent.com/Krushon/VPN_scripts/master/openvpn-server-script/1_script-upgrade.sh
 wget https://raw.githubusercontent.com/Krushon/VPN_scripts/master/openvpn-server-script/2_script-install.sh
