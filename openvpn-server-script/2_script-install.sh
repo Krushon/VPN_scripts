494b60ade96dc6e810935305590405b4f7abb4af
Krushon <email5000@yandex.ru>
2019-11-29 11:36:36 +0300

    Небольшие оптимизации

--- openvpn-server-script/2_script-install.sh
+++ openvpn-server-script/2_script-install.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-# Устанавливаем пакеты OpenVPN, MidnightCommander, почтовые утилиты и NTP для синхронизации часов.
 SECONDS=0
 printf "\033c"
-echo "Устанавливаем необходимое ПО..."
+echo "Install requirements packets..."
 apt-get update
-apt-get install openvpn mc ntpdate ntp net-tools mailutils mutt -y
+apt-get install openvpn mc chrony nftables mutt mailutils -y
 apt-get autoclean && apt-get clean
-/etc/init.d/ntp stop && ntpdate pool.ntp.org && /etc/init.d/ntp start
+systemctl enable chrony
 # Изменяем временную зону
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 # Готовимся к генерации сертификатов
 mkdir /etc/openvpn/easy-rsa
-cp -r /usr/share/easy-rsa/* /etc/openvpn/easy-rsa
-ln -s /etc/openvpn/easy-rsa/openssl-1.0.0.cnf /etc/openvpn/easy-rsa/openssl.cnf
-# Разрешаем форвардить пакеты из одной сети в другую.
-sed -i '28s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+# Разрешаем переcылать пакеты из одной сети в другую.
+sed -i '28s/.*net.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 echo
 echo -e "***** Script \033[33;1m2\033[0m of \033[33;1m3\033[0m COMPLETED in $SECONDS seconds *****"
 echo
