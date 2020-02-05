86774920654e6c97c02532d2b3d6f5d1e94b9b62
Krushon <email5000@yandex.ru>
2020-02-05 17:27:23 +0300

    Небольшие улучшения, некоторая оптимизация

--- vds-script-l2tp/3_script-conf.sh
+++ vds-script-l2tp/3_script-conf.sh
@@ -15,27 +15,29 @@ mv /etc/ipsec.conf /etc/ipsec.conf.bak && chmod 644 /etc/ipsec.conf.bak
 touch /etc/ipsec.conf && chmod 755 /etc/ipsec.conf
 echo -en "version 2.0\nconfig setup\n" >> /etc/ipsec.conf
 echo -en "\tnat_traversal=yes\n\tvirtual_private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12\n" >> /etc/ipsec.conf
-echo -en "\toe=off\n\tprotostack=netkey\n\tnhelpers=0\n\t;\n\tconn L2TP-PSK-NAT\n" >> /etc/ipsec.conf
-echo -en "\trightsubnet=vhost:%priv\n\talso=L2TP-PSK-noNAT\n\t;\n\tconn L2TP-PSK-noNAT\n" >> /etc/ipsec.conf
-echo -en "\tauthby=secret\n\tpfs=no\n\tauto=add\n\tkeyingtries=3\n\trekey=no\n" >> /etc/ipsec.conf
-echo -en "\tdpddelay=30\n\tdpdtimeout=120\n\tdpdaction=clear\n\tikelifetime=8h\n" >> /etc/ipsec.conf
+echo -en "\toe=off\n\tprotostack=netkey\n\tnhelpers=0\n" >> /etc/ipsec.conf
+echo -en "conn L2TP-PSK-NAT\n\trightsubnet=vhost:%priv\n\talso=L2TP-PSK-noNAT\n" >> /etc/ipsec.conf
+echo -en "conn L2TP-PSK-noNAT\n\tauthby=secret\n\tpfs=no\n\tauto=add\n\tkeyingtries=3\n" >> /etc/ipsec.conf
+echo -en "\trekey=no\n\tdpddelay=30\n\tdpdtimeout=120\n\tdpdaction=clear\n\tikelifetime=8h\n" >> /etc/ipsec.conf
 echo -en "\tkeylife=1h\n\ttype=transport\n\tleft=$VDSIP\n\tleftprotoport=17/1701\n" >> /etc/ipsec.conf
-echo -en "\tright=%any\n\trightprotoport=17/%any\nforceencaps=yes\n" >> /etc/ipsec.conf
-# Указываем PSK ключ для ipsec
+echo -en "\tright=%any\n\trightprotoport=17/%any\n#forceencaps=yes\n" >> /etc/ipsec.conf
+
+# Указываем PSK ключ для1 ipsec
 touch /etc/ipsec.secrets
-echo -en "$VDSIP %any: PSK \042$PSK-KEY\042" >> /etc/ipsec.secrets
+quote=$'\042'
+echo -en "$VDSIP %any: PSK $quote$PSKKEY$quote" >> /etc/ipsec.secrets
 
 ## Настройка l2tp
 mv /etc/xl2tpd/xl2tpd.conf /etc/xl2tpd/xl2tpd.conf.bak
 touch /etc/xl2tpd/xl2tpd.conf
-echo -en "[global]\n\tlisten-addr = $VDSIP\n\tport = 1701\n\tipsec saref = no\n" >> /etc/ixl2tpd/xl2tpd.conf
-echo -en "\tdebug tunnel = yes\n\tdebug avp = yes\n\tdebug packet = yes\n" >> /etc/ixl2tpd/xl2tpd.conf
-echo -en "\tdebug network = yes\n\tdebug state = yes\n\tauth file = /etc/ppp/chap-secrets\n" >> /etc/ixl2tpd/xl2tpd.conf
-echo -en "\t;\n\t[lns default]\n\tip range = 172.16.254.1-172.16.254.253 ; Диапазон IP-адресов, которые выдаются подключающимся клиентам\n" >> /etc/ixl2tpd/xl2tpd.conf
-echo -en "\tlocal ip = 172.16.254.254 ; Локальный IP-адрес сервера для VPN-клиентов\n" >> /etc/ixl2tpd/xl2tpd.conf
-echo -en "\trefuse chap = yes\n\trefuse pap = yes\n\trequire authentication = yes\n" >> /etc/ixl2tpd/xl2tpd.conf
-echo -en "\tppp debug = yes\n\tpppoptfile = /etc/ppp/options.xl2tpd\n\tlength bit = yes\n" >> /etc/ixl2tpd/xl2tpd.conf
-echo -en "\tname = VPN\nassign ip = yes\n" >> /etc/ixl2tpd/xl2tpd.conf
+echo -en "[global]\n\tlisten-addr = $VDSIP\n\tport = 1701\n\tipsec saref = no\n" >> /etc/xl2tpd/xl2tpd.conf
+echo -en "\tdebug tunnel = yes\n\tdebug avp = yes\n\tdebug packet = yes\n" >> /etc/xl2tpd/xl2tpd.conf
+echo -en "\tdebug network = yes\n\tdebug state = yes\n\tauth file = /etc/ppp/chap-secrets\n" >> /etc/xl2tpd/xl2tpd.conf
+echo -en "\t;\n\t[lns default]\n\tip range = 172.16.254.1-172.16.254.253\n" >> /etc/xl2tpd/xl2tpd.conf
+echo -en "\tlocal ip = 172.16.254.254\n\trefuse chap = yes\n\trefuse pap = yes\n" >> /etc/xl2tpd/xl2tpd.conf
+echo -en "\trequire authentication = yes\n\tppp debug = yes\n" >> /etc/xl2tpd/xl2tpd.conf
+echo -en "\tpppoptfile = /etc/ppp/options.xl2tpd\n\tlength bit = yes\n" >> /etc/xl2tpd/xl2tpd.conf
+echo -en "\tname = VPN\nassign ip = yes\n" >> /etc/xl2tpd/xl2tpd.conf
 
 ## Настройка ppp
 touch /etc/ppp/options.xl2tpd
@@ -58,7 +60,7 @@ sed -i '28s/.*net.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 # Отключаем ICMP send_ и accept_redirects
 sed -i '44s/.*net.*/net.ipv4.conf.all.accept_redirects = 0/' /etc/sysctl.conf
 sed -i '52s/.*net.*/net.ipv4.conf.all.send_redirects = 0/' /etc/sysctl.conf
-echo -en "\nnet.ipv4.conf.default.send_redirects = 0\nnet.ipv4.conf.default.accept_redirects = 0\n" >> /etc/sysctl.conf
+echo -en "net.ipv4.conf.default.send_redirects = 0\nnet.ipv4.conf.default.accept_redirects = 0\n" >> /etc/sysctl.conf
 echo -en "net.ipv4.conf.$NET.send_redirects = 0\nnet.ipv4.conf.$NET.accept_redirects = 0\n" /etc/sysctl.conf
 
 ## Перезапуск служб
