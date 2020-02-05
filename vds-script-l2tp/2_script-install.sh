6d8e6a1fd17eb6c5ed8850790dc354547a2a94dc
Krushon <email5000@yandex.ru>
2020-02-05 14:28:42 +0300

    Добавлены почтовые пакеты

--- vds-script-l2tp/2_script-install.sh
+++ vds-script-l2tp/2_script-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 SECONDS=0
 printf "\033c"
-echo "Install requirements packets..."
+echo "Install requirements packages..."
 apt-get update
-apt-get install mc chrony fail2ban -y
+apt-get install mc chrony fail2ban mutt mailutils gpgsm -y
 apt-get install iptables-persistent xl2tpd libgmp3-dev gawk flex bison make libc6-dev devscripts libssl-dev -y
 apt-get autoclean && apt-get clean
 systemctl enable chrony
+# Изменяем временную зону
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 # устанавливаем openswan
 wget https://download.openswan.org/openswan/openswan-latest.tar.gz
 tar -xvzf openswan-latest.tar.gz
@@ -15,11 +17,6 @@ openswandir=`find . -type d -name 'openswan' | awk -F/ 'NR == 1{print$2}'`
 cd $openswandir
 make programs
 make install
-
-# Изменяем временную зону
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-# Разрешаем переcылать пакеты из одной сети в другую.
-sed -i '28s/.*net.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 echo
 echo -e "***** Script \033[33;1m2\033[0m of \033[33;1m3\033[0m COMPLETED in $SECONDS seconds *****"
 echo
