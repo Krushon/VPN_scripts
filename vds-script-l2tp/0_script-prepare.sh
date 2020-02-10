277ed12d66b9376865fd6b8a160b40af1bfbaae5
Krushon <email5000@yandex.ru>
2020-02-10 13:40:54 +0300

    Добавлена проверка необходимых модулей ядра

--- vds-script-l2tp/0_script-prepare.sh
+++ vds-script-l2tp/0_script-prepare.sh
@@ -17,6 +17,89 @@ else
     echo -e "\e[31mTUN/TAP отключено. Обратитесь к вашему провайдеру VPS/VDS, чтобы включить.\e[0m"
     exit 1
 fi
+
+lsmodafkey=`lsmod | grep af_key`
+lsmodesp4=`lsmod | grep esp4`
+lsmodxfrm4=`lsmod | grep xfrm4_mode_tunnel`
+findmodafkey=`find /lib/modules -name *.ko | grep af_key | awk -F/ 'print $8}'`
+findmodesp4=`find /lib/modules -name *.ko | grep esp4 | awk -F/ '{print $8}'` 
+findmodxfrm4=`find /lib/modules -name *.ko | grep xfrm4_mode_tunnel | awk -F/ '{print $8}'`
+
+#Проверяем подключение модуля ядра af_key
+if [[ -n $lsmodafkey ]]
+	then
+		echo -e "\e[1;32mМодуль af_key включен.\e[0m"
+elif [[ -z $lsmodafkey ]] && [[ -n $findmodafkey ]]
+	then
+		echo -e "\e[1;33mМодуль af_key установлен, но не включен. Включить его (Y/n)?\e[0m"
+			read answer1
+			case "$answer1" in
+				y|Y) modprobe af_key
+				echo -e "\e[1;32mМодуль af_key включен.\e[0m"
+				;;
+				n|N) echo -e "\e[1;31mОтмена. Модуль af_key не включен.\e[0m"
+				exit 1
+				;;
+				*) echo "Ничего не ввели. Выполняется действие по умолчанию - включение модуля."
+				modprobe af_key
+				echo -e "\e[1;32mМодуль af_key включен.\e[0m"
+				;;
+			esac
+else
+    echo -e "\e[1;31mМодуль af_key не загружен.\e[0m"
+    exit 1
+fi
+
+#Проверяем подключение модуля ядра esp4
+if [[ -n $lsmodesp4 ]]
+	then
+		echo -e "\e[1;32mМодуль esp4 включен.\e[0m"
+elif [[ -z $lsmodesp4 ]] && [[ -n $findmodesp4 ]]
+	then
+		echo -e "\e[1;33mМодуль esp4 установлен, но не включен. Включить его (Y/n)?\e[0m"
+			read answer2
+			case "$answer2" in
+				y|Y) modprobe esp4
+				echo -e "\e[1;32mМодуль esp4 включен.\e[0m"
+				;;
+				n|N) echo -e "\e[1;31mОтмена. Модуль esp4 не включен.\e[0m"
+				exit 1
+				;;
+				*) echo "Ничего не ввели. Выполняется действие по умолчанию - включение модуля."
+				modprobe esp4
+				echo -e "\e[1;32mМодуль esp4 включен.\e[0m"
+				;;
+			esac
+else
+    echo -e "\e[1;31mМодуль esp4 не загружен.\e[0m"
+    exit 1
+fi
+
+#Проверяем подключение модуля ядра xfrm4_mode_tunnel
+if [[ -n $lsmodxfrm4 ]]
+	then
+		echo -e "\e[1;32mМодуль xfrm4_mode_tunnel включен.\e[0m"
+elif [[ -z $lsmodxfrm4 ]] && [[ -n $findmodxfrm4 ]]
+	then
+		echo -e "\e[1;33mМодуль xfrm4_mode_tunnel установлен, но не включен. Включить его (Y/n)?\e[0m"
+			read answer3
+			case "$answer3" in
+				y|Y) modprobe xfrm4_mode_tunnel
+				echo -e "\e[1;32mМодуль xfrm4_mode_tunnel включен.\e[0m"
+				;;
+				n|N) echo -e "\e[1;31mОтмена. Модуль xfrm4_mode_tunnel не включен.\e[0m"
+				exit 1
+				;;
+				*) echo "Ничего не ввели. Выполняется действие по умолчанию - включение модуля."
+				modprobe xfrm4_mode_tunnel
+				echo -e "\e[1;32mМодуль xfrm4_mode_tunnel включен.\e[0m"
+				;;
+			esac
+else
+    echo -e "\e[1;31mМодуль xfrm4_mode_tunnel не загружен.\e[0m"
+    exit 1
+fi
+
 # Очистка файла motd
 mv /etc/motd /etc/motd.bak
 touch /etc/motd && chmod 664 /etc/motd
