417bcd304e792f245a9edfdd9990b0b8332e1318
Krushon <email5000@yandex.ru>
2022-07-12 11:27:36 +0300

    Актуализированы штучки

--- vds-script-vpn-deb11/1_script-upgrade.sh
+++ vds-script-vpn-deb11/1_script-upgrade.sh
@@ -3,44 +3,26 @@
 SECONDS=0
 printf "\033c"
 ver=`cat /etc/*-release | grep VERSION_ID | awk -F= '{print $2}'`
-#  If the version is 8 then update to 11. Если версия 8, то обновляем до 11.
-if [ $ver = '"8"' ]
-  then
-    echo "OS version is 8. Upgrading to 11..."
-    cp /etc/apt/sources.list /etc/apt/sources.list_backup
-    sed -i 's/jessie/bullseye/g' /etc/apt/sources.list
-    apt-get update
-    apt-get upgrade -y
-    apt-get dist-upgrade -y
-    apt autoremove -y
-#  If 9 then update to 11. Если версия 9, то обновляем до 11.
-elif [ $ver = '"9"' ]
-  then
-    echo "OS version is 9. Upgrading to 11..."
-    cp /etc/apt/sources.list /etc/apt/sources.list_backup
-    sed -i 's/stretch/bullseye/g' /etc/apt/sources.list
-    apt-get update
-    apt-get upgrade -y
-    apt-get dist-upgrade -y
-    apt autoremove -y
-#  If 10 then update to 11. Если версия 10, то обновляем до 11.
+# 8 - Support over. Поддержка версии 8 (jessie) закончилась 30.06.2020.
+# 9 - Support over. Поддержка версии 9 (stretch) закончилась 30.06.2022.
+# If 10 then update to 11. Если версия 10, то обновляем до 11.
 elif [ $ver = '"10"' ]
   then
     echo "OS version is 10. Upgrading to 11..."
     cp /etc/apt/sources.list /etc/apt/sources.list_backup
     sed -i 's/buster/bullseye/g' /etc/apt/sources.list
-    apt-get update
-    apt-get upgrade -y
-    apt-get dist-upgrade -y
+    apt update
+    apt upgrade -y
+    apt dist-upgrade -y
     apt autoremove -y
 #  If 11 then update. Если версия 11, то просто всё обновляем.
 elif [ $ver = '"11"' ]
   then
     echo "Updating system..."
-    apt-get update
-    apt-get upgrade -y
+    apt update
+    apt upgrade -y
     apt autoremove -y
-# If version is not 8-11 then break. Если версия не 8, 9, 10, 11 и не debian, то скрипт завершается и потребуется ручное вмешательство.
+# If version is not 8-11 then break. Если версия не 10, 11 и не debian, то скрипт завершается и потребуется ручное вмешательство.
   else
     echo -e "\e[31mSomething wnet wrong. Intervention is required.\e[0m"
     echo -e "\e[31mЧто-то пошло не так. Требуется вмешательство.\e[0m"
--- vds-script-vpn-deb11/2_script-install.sh
+++ vds-script-vpn-deb11/2_script-install.sh
@@ -2,9 +2,9 @@
 SECONDS=0
 printf "\033c"
 echo "Install requirements packets..."
-apt-get update
-apt-get install openvpn asterisk mc chrony fail2ban nftables -y
-apt-get autoclean && apt-get clean
+apt update
+apt install openvpn asterisk mc screenfetch chrony fail2ban nftables -y
+apt autoclean && apt-get clean
 systemctl enable chrony
 # Changing the time zone. Изменяем временную зону
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
@@ -15,3 +15,4 @@ sed -i '28s/.*net.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 echo
 echo -e "***** Script \033[33;1m2\033[0m of \033[33;1m3\033[0m COMPLETED in $SECONDS seconds *****"
 echo
+w
