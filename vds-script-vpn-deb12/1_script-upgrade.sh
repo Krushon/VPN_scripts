e47b38302515c397f24f4710efc0db39e7d8df88
krushon <email5000@yandex.ru>
2023-09-20 15:32:03 +0300

    убрана поддержка 10-й версии

--- vds-script-vpn-deb12/1_script-upgrade.sh
+++ vds-script-vpn-deb12/1_script-upgrade.sh
@@ -3,28 +3,20 @@
 SECONDS=0
 printf "\033c"
 ver=`cat /etc/*-release | grep VERSION_ID | awk -F= '{print $2}'`
-# 8 - Support is over. Поддержка версии 8 (jessie) закончилась 30.06.2020.
-# 9 - Support is over. Поддержка версии 9 (stretch) закончилась 30.06.2022.
-# 10- Support over 30.06.2024. Поддержка версии 10 (buster) закончится 30.06.2024.
-# If 10 then update to 11. Если версия 10, то обновляем до 11.
-if [ $ver = '"10"' ]
-  then
-    echo "OS version is 10. Upgrading to 11..."
-    cp /etc/apt/sources.list /etc/apt/sources.list.bak
-    sed -i 's/buster/bookworm/g' /etc/apt/sources.list
-    apt update
-    apt upgrade -y
-    apt dist-upgrade -y
-    apt autoremove -y
+# 9  - Support is over. Поддержка версии 9 (stretch) закончилась 30.06.2022.
+# 10 - Support over 30.06.2024. Поддержка версии 10 (buster) закончится 30.06.2024.
 # If 11 then update to 12. Если версия 11, то обновляем до 12.
-elif [ $ver = '"11"' ]
+if [ $ver = '"11"' ]
   then
+    #выгружаем список установленных пакетов
+    dpkg -l > ~/dpkg-full.list
     echo "OS version is 11. Upgrading to 12..."
     cp /etc/apt/sources.list /etc/apt/sources.list.bak
     sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
     apt update
     apt upgrade -y
     apt dist-upgrade -y
+    apt full-upgrade -y
     apt autoremove -y
 #  If 12 then update. Если версия 12, то просто всё обновляем.
 elif [ $ver = '"12"' ]
@@ -33,7 +25,7 @@ elif [ $ver = '"12"' ]
     apt update
     apt upgrade -y
     apt autoremove -y    
-# If version is not 10,11,12 and not debian then break. Если версия не 10, 11, 12 и не debian, то скрипт завершается и потребуется ручное вмешательство.
+# If version is not 11,12 and not debian then break. Если версия не 11, 12 и не debian, то скрипт завершается и потребуется ручное вмешательство.
   else
     echo -e "\e[31mSomething went wrong. Intervention is required.\e[0m"
     echo -e "\e[31mЧто-то пошло не так. Требуется вмешательство.\e[0m"
